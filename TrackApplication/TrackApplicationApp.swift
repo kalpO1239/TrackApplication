9e474aca2438b3b54ba8127d485761e2a955c2ad
veegskaps <kalp.ostawal1@gmail.com>
2024-12-22 20:11:16 -0500

    Initial Commit

---
+++ TrackApplication/TrackApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackApplicationApp.swift
+//  TrackApplication
+//
+//  Created by Kalp Ostawal on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrackApplicationTests/TrackApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrackApplicationTests.swift
+//  TrackApplicationTests
+//
+//  Created by Kalp Ostawal on 12/22/24.
+//
+
+import Testing
+@testable import TrackApplication
+
+struct TrackApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
